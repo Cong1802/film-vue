0993bd1c07c029efcad443a19835c4b13fc04a62
Cong1802 <conguyen6262@gmail.com>
2023-01-04 22:49:55 +0700

    ok

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+        <meta name="theme-color" content="#234556">
+        <meta http-equiv="Content-Language" content="vi" />
+        <meta content="VN" name="geo.region" />
+        <meta name="DC.language" scheme="utf-8" content="vi" />
+        <meta name="language" content="Việt Nam">
+        
+  
+        <link rel="shortcut icon" href="https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png" type="image/x-icon" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+        <title>Phim hay 2021 - Xem phim hay nhất</title>
+        <meta name="description" content="Phim hay 2021 - Xem phim hay nhất, xem phim online miễn phí, phim hot , phim nhanh" />
+        <link rel="canonical" href="">
+        <link rel="next" href="" />
+        <meta property="og:locale" content="vi_VN" />
+        <meta property="og:title" content="Phim hay 2020 - Xem phim hay nhất" />
+        <meta property="og:description" content="Phim hay 2020 - Xem phim hay nhất, phim hay trung quốc, hàn quốc, việt nam, mỹ, hong kong , chiếu rạp" />
+        <meta property="og:url" content="" />
+        <meta property="og:site_name" content="Phim hay 2021- Xem phim hay nhất" />
+        <meta property="og:image" content="" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="55" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+       
+        <link rel='dns-prefetch' href='//s.w.org' />
+        
+        <link rel='stylesheet' id='bootstrap-css' href={{ asset('/site/css/bootstrap.min.css') }} media='all' />
+        <link rel='stylesheet' id='style-css' href={{ asset('/site/css/style.css') }} media='all' />
+        <link rel='stylesheet' id='wp-block-library-css' href={{ asset('/site/css/style.min.css') }} media='all' />
+        <link rel='stylesheet' id='wp-block-library-css' href={{ asset('/site/css/custom.css') }} media='all' />
+        <script type='text/javascript' src='js/jquery.min.js' id='halim-jquery-js'></script>
+        <style type="text/css" id="wp-custom-css">
+           .textwidget p a img {
+           width: 100%;
+           }
+        </style>
+        <style>
+        .float-ck { position: fixed; bottom: 0px; z-index: 9}
+        * html .float-ck /* IE6 position fixed Bottom */{position:absolute;bottom:auto;top:expression(eval (document.documentElement.scrollTop+document.docum entElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0))) ;}
+        #hide_float_left a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;float: left;}
+        #hide_float_left_m a {background: #0098D2;padding: 5px 15px 5px 15px;color: #FFF;font-weight: 700;}
+        span.bannermobi2 img {height: 70px;width: 300px;}
+        #hide_float_right a { background: #01AEF0; padding: 5px 5px 1px 5px; color: #FFF;float: left;}
+        </style>
+        <style>#header .site-title {background: url(https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png) no-repeat top left;background-size: contain;text-indent: -9999px;}</style>
+     </head>
+    <body class="home blog halimthemes halimmovies" data-masonry="">
+        <div id="app">
+            <header-site></header-site>
+            <main-site></main-site>
+            <footer-site></footer-site>
+        </div>
+        <script type='text/javascript' src={{ asset('/site/js/bootstrap.min.js') }} id='bootstrap-js'></script>
+        <script type='text/javascript' src={{ asset('/site/js/owl.carousel.min.js') }} id='carousel-js'></script>
+        <script type='text/javascript' src={{ asset('/site/js/halimtheme-core.min.js') }} id='halim-init-js'></script>
+        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
